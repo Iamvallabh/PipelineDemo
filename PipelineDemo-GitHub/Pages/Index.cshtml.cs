d743fe41c80167d3854632d214bbcf7230af455b
mukesh <mukesh@SelfHostedVM>
2026-03-27 09:32:11 +0000

    Add project files.

---
+++ PipelineDemo-GitHub/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PipelineDemo_GitHub.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
